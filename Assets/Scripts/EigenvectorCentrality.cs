80685545f4a073610be0d9931edde74158aa823c
Keyansn <keyansn@gmail.com>
2017-11-17 02:08:57 +0000

    Eigenvector Centrality (Needs to be normalized and polished)

---
+++ Assets/Scripts/EigenvectorCentrality.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+public class EigenvectorCentrality : MonoBehaviour {
+	
+	public int degree;
+	public int ec;
+	//GameObject[] connectednodelist;
+	/*
+	public int ec
+	{
+		get { return EC; }
+		set { EC = value; }
+	}
+
+	
+		THIS WILL NEED TO BE CALLED BY THE BUTTON PRESSES RATHER THAN BEING ATTACHED TO THE PREFAB AND RUNNING ON UPDATE
+
+		Also needs to be normalised.
+*/
+
+
+
+	// Use this for initialization
+	void Start () {
+		degree = 0;
+		ec = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CountCentrality();
+	}
+
+	void CountCentrality(){
+		GameObject[] linklist;
+		int link_length;
+
+		linklist = GameObject.FindGameObjectsWithTag("link");
+
+		//Find how many nodes & links there are
+		link_length = linklist.Length;
+
+		ec = 0;
+		degree = 0;
+		for (int i = 0; i < link_length; i++)
+			{
+			if ((linklist[i].GetComponent<Link>().source == this.gameObject)||linklist[i].GetComponent<Link>().target == this.gameObject)
+				{
+				degree = degree + 1;
+
+				}
+
+			if ((linklist[i].GetComponent<Link>().source == this.gameObject)||linklist[i].GetComponent<Link>().target == this.gameObject)
+			{
+				if ((linklist[i].GetComponent<Link>().source != this.gameObject)){
+
+					EigenvectorCentrality eScript = linklist[i].GetComponent<Link>().source.GetComponent<EigenvectorCentrality>();
+					print(eScript.degree);
+					ec = ec + eScript.degree;
+
+
+				}
+
+				if ((linklist[i].GetComponent<Link>().target != this.gameObject)){
+
+					EigenvectorCentrality eScript = linklist[i].GetComponent<Link>().target.GetComponent<EigenvectorCentrality>();
+					print(eScript.degree);
+					ec = ec + eScript.degree;
+
+
+				}
+			}
+
+
+			}
+
+	}
+
+}
+
+
